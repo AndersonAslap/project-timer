9c0234d8985c11b844a84ffd974658cdf2af9020
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-07-20 18:20:50 -0300

    feat: validando formularios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   HomeContainer,
@@ -11,10 +13,20 @@ import {
   MinutesAmountInput,
 } from './styles'
 
-export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'Mínimo 5 minutos')
+    .max(60, 'Máximo 60 minutos'),
+})
 
-  function handleCreateNewCicle(data) {
+export function Home() {
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
+
+  function handleCreateNewCicle(data: any) {
     console.log(data)
   }
 
